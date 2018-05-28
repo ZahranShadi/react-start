6b6d5b555fa7c7e4bba803e4bbab4f6377b385d1
ShadiZahran <shadi.zahran@fer.hr>
2018-05-28 11:16:02 +0200

    Created initial files with text output

---
+++ react-app/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            React app!
+         </div>
+      );
+   }
+}
+export default App;
